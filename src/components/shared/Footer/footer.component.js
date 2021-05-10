ce092195ecd7957fea3695a6bc9404eafe4ed0a2
Deva <you@example.comdevabalagurusamy@gmail.com>
2021-05-10 09:52:19 +0530

    first commit

---
+++ src/components/shared/Footer/footer.component.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function Footer(props) {
+    return (
+        <Fragment>
+            <hr />
+            <small>&copy; devasarathyB</small>
+        </Fragment>
+    );
+}
+
+export default Footer;
---
+++ src/components/shared/Header/header.component.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function Header(props) {
+    return (
+        <Fragment>
+            <h1 align="center">Register Here</h1>
+            <hr />
+        </Fragment>
+    );
+}
+
+export default Header;
